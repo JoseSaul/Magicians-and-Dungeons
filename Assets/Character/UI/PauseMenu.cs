7b274ecf362b108e353a2952919cc39a16ec7ae7
JoseSaul <40270646+JoseSaul@users.noreply.github.com>
2020-05-14 13:01:52 +0000

    Finished Pausa Menu

---
+++ Assets/Character/UI/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Character.UI
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        [SerializeField] private GameObject panelPause, panelAreYouSure;
+
+        public void ResumeGame()
+        {
+            this.gameObject.SetActive(false);
+            Time.timeScale = 1;   
+        }
+
+        public void ExitAreYouSure()
+        {
+            panelPause.SetActive(false);
+            panelAreYouSure.SetActive(true);
+        }
+
+        public void ReturnToPauseMenu()
+        {
+            panelAreYouSure.SetActive(false);
+            panelPause.SetActive(true);
+        }
+
+        public void ExitToMainMenu()
+        {
+            print("Salgo al menu");
+        }
+        
+    }
+}

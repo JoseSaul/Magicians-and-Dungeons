760c025a456e713b9ed7337c119712bc964cbef5
JoseSaul <40270646+JoseSaul@users.noreply.github.com>
2020-07-28 13:40:45 +0000

    LoadMap

---
+++ Assets/Scenes/Structures/LoadMap.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Scenes.Structures
+{
+    public class LoadMap : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+        
+    }
+}
